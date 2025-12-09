4efd6790035714f837657afe5a64f2e951e89a44
Kartik <kartiktripathi103@gmail.com>
2025-12-09 15:45:57 +0530

    Added required CSS

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
-const choices = ["rock", "paper", "scissors"];
-const playerDisplay = document.getElementById("playerDisplay"); //player choice display
-const compDisplay = document.getElementById("compDisplay");//comp choice display
-const resultDisplay = document.getElementById("resultDisplay");//result
-const playerScoreDisplay = document.getElementById("playerScoreDisplay");//pllayer score
-const compScoreDisplay = document.getElementById("compScoreDisplay");//comp score
+const choices = ["Rock", "Paper", "Scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const compDisplay = document.getElementById("compDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const compScoreDisplay = document.getElementById("compScoreDisplay");
 let playerScore = 0;
 let compScore = 0;
 function playGame(playerChoice){
-    const compChoice = choices[Math.floor(Math.random()*3)]; // 0-2 tak ke result ke liye
+    const compChoice = choices[Math.floor(Math.random()*3)];
     let result = "";
     if(playerChoice === compChoice){
-        result = "It's a Tie!!";
+        result = "It's a Tie!";
     }
     else {
     switch (playerChoice) {
-        case "rock":
-            if (compChoice === "scissors") {
-                result = "You Win!!";
+        case "Rock":
+            if (compChoice === "Scissors") {
+                result = "You Win!";
             } else {
-                result = "You Lose!!";
+                result = "You Lose!";
             }
             break;
-        case "paper":
-            if (compChoice === "rock") {
-                result = "You Win!!";
+        case "Paper":
+            if (compChoice === "Rock") {
+                result = "You Win!";
             } else {
-                result = "You Lose!!";
+                result = "You Lose!";
             }
             break;
-        case "scissors":
-            if (compChoice === "paper") {
-                result = "You Win!!";
+        case "Scissors":
+            if (compChoice === "Paper") {
+                result = "You Win!";
             } else {
-                result = "You Lose!!";
+                result = "You Lose!";
             }
             break;
     }
 }
-    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    playerDisplay.textContent = `Player: ${playerChoice}`;
     compDisplay.textContent = `Computer: ${compChoice}`;
     resultDisplay.textContent = result;
-    resultDisplay.classList.remove("green", "red");
+    resultDisplay.classList.remove("green", "red", "yellow");
     switch(result){
-        case "You Win!!":
+        case "You Win!":
             resultDisplay.classList.add("green");
             playerScore++;
             playerScoreDisplay.textContent = playerScore;
             break;
-        case "You Lose!!":
+        case "You Lose!":
             resultDisplay.classList.add("red");
             compScore++;
             compScoreDisplay.textContent = compScore;
+            break;
+        case "It's a Tie!":
+            resultDisplay.classList.add("yellow");
             break;
     }
 }
